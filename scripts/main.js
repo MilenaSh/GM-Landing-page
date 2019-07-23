c1555f7e234b1aaeb273be220ffa49fba3dc09fd
Milena Shishkova <milena@a-triks.com>
2019-07-23 19:43:48 +0300

    Final version - pretesting

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,56 @@
 $('document').ready(function() {
-    // Select all links with hashes
     $('a[href*="#"]')
-        // Remove links that don't actually link to anything
         .not('[href="#"]')
         .not('[href="#0"]')
         .click(function(event) {
-            // On-page links
+            // 
             if (
                 location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
                 location.hostname == this.hostname
             ) {
-                // Figure out element to scroll to
+
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
+   
                 if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
+ 
                     event.preventDefault();
                     $('html, body').animate({
                         scrollTop: target.offset().top - 120
                     }, 1000, function() {
-                        // Callback after animation
-                        // Must change focus!
+       
                         var $target = $(target);
                         $target.focus();
-                        if ($target.is(":focus")) { // Checking if the target was focused
+                        if ($target.is(":focus")) { 
                             return false;
                         } else {
-                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                            $target.focus(); // Set focus again
+                            $target.attr('tabindex', '-1'); 
+                            $target.focus(); 
                         };
                     });
                 }
             }
         });
 
-    $('.image-holder img').on('click', loadContent);
+    $('.image-holder').on('click', loadContent);
 })
 
 function loadContent(ev) {
+    
+    let $selected = $(this);
+    let descriptionClass = $selected.attr('data-details');
     let $sideDescription = $('.side-description span');
-    let $selected = $(ev.target).parent();
+    let $description = $('.' + descriptionClass);
+    let description = $description.html();
+
     let windowWidth = $(window).width();
     $('.image-holder').removeClass('selected');
-
-    if (windowWidth < 768) {
-        let selectedClass = $($selected).attr('data-details');
-        $($selected).parent().parent().find('.' + selectedClass).toggle();
-        $sideDescription.html('');
-    } else {
-        //  console.log($($selected).parent().find('.details').html());
-        let details = $($selected).parent().find('.details').html();
-        $sideDescription.html(details);
-    }
     $selected.addClass('selected');
+
+    if (windowWidth > 768) {
+        $sideDescription.html(description);
+     } else {
+        $('.details').css('display', 'none');
+        $description.slideToggle();
+     }
 }
